b2ffbddb8b304878c6ea31ed5df7e0a633824ace
rostirolla55 <ruggero.rostirolla@gmail.com>
2026-01-02 00:12:23 +0100

    upd pytho  add_page.py salvata copia come add_page_v3.9.6

--- main.js
+++ main.js
@@ -49,7 +49,8 @@ const POIS_LOCATIONS = [
     // Opera di Agostino Carracci - 44.498910, 11.342241
     { id: 'pioggia3', lat: 44.498910, lon: 11.342241, distanceThreshold: 120 },
     // Tanari_11.jpg
-    { id: 'lastre', lat: 44.49925278, lon: 11.34074444, distanceThreshold: 50 }
+    { id: 'lastre', lat: 44.49925278, lon: 11.34074444, distanceThreshold: 50 },
+    { id: 'bsmariamaggiore', lat: 44.49806368372069, lon: 11.34192628931731, distanceThreshold: 50 }
 // ** MARKER: START NEW POIS **
 ];
 
@@ -321,7 +322,8 @@ async function loadContent(lang) {
                 { id: 'navPioggia3', key: 'navPioggia3', base: 'pioggia3' },
                 { id: 'navManifattura', key: 'navManifattura', base: 'manifattura' },
                 { id: 'navPittoriCarracci', key: 'navPittoriCarracci', base: 'pittoricarracci' },
-                { id: 'navCavaticcio', key: 'navCavaticcio', base: 'cavaticcio' }
+                { id: 'navCavaticcio', key: 'navCavaticcio', base: 'cavaticcio' },
+    { id: 'navbsmariamaggiore', key: 'navbsmariamaggiore', base: 'bsmariamaggiore' }
                 // ** MARKER: START NEW NAV LINKS **
             ];
 
