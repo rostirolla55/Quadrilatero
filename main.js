8a493b7020367299c9da2408a6f1b00533daaf85
rostirolla55 <ruggero.rostirolla@gmail.com>
2026-02-05 11:51:47 +0100

    test poi 10 metri

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ const POIS_LOCATIONS = [
     { id: 'cavaticcio', lat: 44.50018, lon: 11.33807, distanceThreshold: 50 },
     { id: 'bsmariamaggiore', lat: 44.49806368372069, lon: 11.34192628931731, distanceThreshold: 50 },
     { id: 'graziaxx', lat: 44.5006638888889, lon: 11.3407694444444, distanceThreshold: 50 },
-    { id: 'pugliole', lat: 44.5001944444444, lon: 11.3399861111111, distanceThreshold: 50 },
-    { id: 'carracci', lat: 44.4999972222222, lon: 11.3403888888889, distanceThreshold: 50 },
+    { id: 'pugliole', lat: 44.5001944444444, lon: 11.3399861111111, distanceThreshold: 10 },
+    { id: 'carracci', lat: 44.4999972222222, lon: 11.3403888888889, distanceThreshold: 10 },
     { id: 'lastre', lat: 44.49925278, lon: 11.34074444, distanceThreshold: 50 },
     { id: 'chiesasbene', lat: 44.501514, lon: 11.343557, distanceThreshold: 120 },
     { id: 'santuariopioggia', lat: 44.49891, lon: 11.342241, distanceThreshold: 120 },
@@ -168,21 +168,21 @@ function updateNavigation(navData, lang) {
     const langSuffix = lang === 'it' ? '-it' : `-${lang}`;
 
     const navLinksData = [
-        { id: 'navHome', key: 'navHome', base: 'index' },
-        { id: 'navManifattura', key: 'navManifattura', base: 'manifattura' },
-        { id: 'navPittoriCarracci', key: 'navPittoriCarracci', base: 'pittoricarracci' },
-        { id: 'navCavaticcio', key: 'navCavaticcio', base: 'cavaticcio' },
-        { id: 'navBSMariaMaggiore', key: 'navBSMariaMaggiore', base: 'bsmariamaggiore' },
-        { id: 'navGraziaxx', key: 'navGraziaxx', base: 'graziaxx' },
-        { id: 'navPugliole', key: 'navPugliole', base: 'pugliole' },
-        { id: 'navCarracci', key: 'navCarracci', base: 'carracci' },
-        { id: 'navLastre', key: 'navLastre', base: 'lastre' },
-        { id: 'navChiesaSBene', key: 'navChiesaSBene', base: 'chiesasbene' },
-        { id: 'navSantuarioPioggia', key: 'navSantuarioPioggia', base: 'santuariopioggia' },
-        { id: 'navPioggia1', key: 'navPioggia1', base: 'pioggia1' },
-        { id: 'navPioggia2', key: 'navPioggia2', base: 'pioggia2' },
-        { id: 'navPioggia3', key: 'navPioggia3', base: 'pioggia3' }
-    ];
+    { id: 'navHome', key: 'navHome', base: 'index' },
+    { id: 'navManifattura', key: 'navManifattura', base: 'manifattura' },
+    { id: 'navPittoriCarracci', key: 'navPittoriCarracci', base: 'pittoricarracci' },
+    { id: 'navCavaticcio', key: 'navCavaticcio', base: 'cavaticcio' },
+    { id: 'navBSMariaMaggiore', key: 'navBSMariaMaggiore', base: 'bsmariamaggiore' },
+    { id: 'navGraziaxx', key: 'navGraziaxx', base: 'graziaxx' },
+    { id: 'navPugliole', key: 'navPugliole', base: 'pugliole' },
+    { id: 'navCarracci', key: 'navCarracci', base: 'carracci' },
+    { id: 'navLastre', key: 'navLastre', base: 'lastre' },
+    { id: 'navChiesaSBene', key: 'navChiesaSBene', base: 'chiesasbene' },
+    { id: 'navSantuarioPioggia', key: 'navSantuarioPioggia', base: 'santuariopioggia' },
+    { id: 'navPioggia1', key: 'navPioggia1', base: 'pioggia1' },
+    { id: 'navPioggia2', key: 'navPioggia2', base: 'pioggia2' },
+    { id: 'navPioggia3', key: 'navPioggia3', base: 'pioggia3' }
+];
 
     navLinksData.forEach(l => {
         const el = document.getElementById(l.id);
