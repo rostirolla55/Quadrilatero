76e70b63127815d88f352662976095a0a74ca638
rostirolla55 <ruggero.rostirolla@gmail.com>
2026-02-05 11:29:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebas
 import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore, doc, onSnapshot, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-const APP_VERSION = '1.2.32 - Fix Menu Toggle & Navigation';
+const APP_VERSION = '1.2.33 - gestione messaggio nessun POI trovato';
 const LANGUAGES = ['it', 'en', 'fr', 'es'];
 const LAST_LANG_KEY = 'Quadrilatero_lastLang';
 let currentLang = 'it';
